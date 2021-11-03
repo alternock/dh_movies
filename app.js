3d8398c281a517ab789289f5664c9a06cd13208c
midoc05 <dominique.croce@gmail.com>
2021-11-02 22:10:49 -0300

    corrección error en app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 //
 const genericCTRL = require("./controllers/generic_ctrl");
 const fianrielCTRL = require("./controllers/fianriel_ctrl");
+const dominicCTRL = require("./controllers/dominic_ctrl");
+const midocCTRL = require("./controllers/midoc_ctrl");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,8 +14,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.resolve(__dirname, "public")));
 
 app.use("/", genericCTRL);
-app.use("/dominic", dominic_ctrl);
-app.use("/midoc", midoc_ctrl);
+app.use("/dominic", dominicCTRL);
+app.use("/midoc", midocCTRL);
 
 app.use("/fianriel", fianrielCTRL);
 
