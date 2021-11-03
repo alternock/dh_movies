e70ddb92a41577cf9512111b9b975c6cf96eb827
esteban18498 <esteban18498@gmail.com>
2021-11-02 22:01:19 -0300

    [add] Fianriel's files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 //
 const genericCTRL = require("./controllers/generic_ctrl");
+const fianrielCTRL = require("./controllers/fianriel_ctrl");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.resolve(__dirname, "public")));
 
 app.use("/", genericCTRL);
+
+app.use("/fianriel", fianrielCTRL);
 
 app.listen(PORT, ()=>{
     console.log("start server");
