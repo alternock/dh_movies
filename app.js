efa0aa16d5c51cdacf9837eaba86fa5535ae8306
alejandrolederman <alejandro.lederman@live.com.ar>
2021-11-02 22:18:18 -0300

    [add] alejandro

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 //
 const genericCTRL = require("./controllers/generic_ctrl");
+const alejandro_ctrl = require("./controllers/alejandro_ctrl");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +13,8 @@ app.use(express.static(path.resolve(__dirname, "public")));
 
 app.use("/", genericCTRL);
 
-app.use("/alejandro", alejanro_ctrl);
+
+app.use("/alejandro", alejandro_ctrl);
 
 app.listen(PORT, ()=>{
     console.log("start server");
